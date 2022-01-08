edafad2b1ff704655497995df6af4d1690cbd944
JonathanThing <jonathancai834@gmail.com>
2022-01-07 23:08:30 -0500

    Testing
    no clue

---
+++ src/Const.java
@@ -0,0 +1,8 @@
+public class Const {
+    public static final int NONE = 111111;
+    public static final int BOXSIZE = 64;
+    public static final int WIDTH = 800;
+	public static final int HEIGHT = 600;
+    
+    private Const() {}
+} 
