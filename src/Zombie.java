61d9913fe9589fe8b08a60c47f2cf0c89808dae9
Wagee-H <97238088+Wagee-H@users.noreply.github.com>
2022-01-14 09:30:33 -0500

    Delete Zombie.java

--- src/Zombie.java
+++
@@ -1,9 +0,0 @@
-import java.awt.image.BufferedImage;
-
-class Zombie extends Enemy {
-
-  Zombie(Vector position, int width, int height, String name, Angle angle, BufferedImage sprite, double health,
-      double speed, Weapon weapon) {
-    super(position, width, height, name, angle, sprite, health, speed, weapon);
-  }
-}
