54207660f80f964694c26b14e7945f3d1b814303
Wagee-H <97238088+Wagee-H@users.noreply.github.com>
2022-01-13 23:49:17 -0500

    Update Const.java

--- Const.java
+++ Const.java
@@ -1,7 +1,8 @@
 public class Const {
-    public static final int NONE = 111111;
-    public static final int BOXSIZE = 64;
-    public static final int WIDTH = 800;
+	
+    	public static final int NONE = 111111;
+    	public static final int BOXSIZE = 64;
+    	public static final int WIDTH = 800;
 	public static final int HEIGHT = 600;
 	public static final int TRUE_WIDTH = WIDTH-62;
 	public static final int TRUE_HEIGHT = HEIGHT+40;
