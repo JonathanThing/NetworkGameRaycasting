1badc8976b1a0169e490df1530698529f231b3b1
341042836 <84483261+341042836@users.noreply.github.com>
2022-01-14 09:30:48 -0500

    Add files via upload

---
+++ src/Zombie.java
@@ -0,0 +1,18 @@
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+
+class Zombie extends Enemy {
+    
+    Zombie(Vector position, int width, int height, String name, Angle angle, BufferedImage sprite, double health, double speed, Weapon weapon) {
+        super(position, width, height, name, angle, sprite, health, speed, weapon); 
+    }  
+    
+    public void attack(Player player){
+        Vector distance = (this.getPosition()).subtract(player.getPosition());
+        Vector hypotenuse = distance.normalized();
+              
+        this.moveLeft(-1 *hypotenuse.getX());      
+        this.moveUp(hypotenuse.getY()); 
+    }
+    
+}
