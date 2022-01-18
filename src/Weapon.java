be4eb62982bd05ce3092401ac19421d3c6f26fcb
ShivanshP1 <97240677+ShivanshP1@users.noreply.github.com>
2022-01-18 11:30:12 -0500

    Update Weapon.java

--- src/Weapon.java
+++ src/Weapon.java
@@ -1,13 +1,18 @@
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 
-class Weapon {
+abstract class Weapon {
 
   private int size;
 
   private double damage;
 
   private double fireRate;
-
+  
+  private long lastFire;
+  
+  private int clipSize;
+  
   public double getFireRate() {
     return this.fireRate;
   }
@@ -15,11 +20,29 @@ class Weapon {
   public int getSize() {
     return this.size;
   }
+  
+  public double getDamage() {
+	return this.damage;
+  }
+  
+  public long getLastFire() {
+	  return this.lastFire;
+  }
+  
+  public int getAmmoSize() {
+	    return this.clipSize;
+	  }
 
-  Weapon(int x, int y, int width, int height, String name, BufferedImage sprite, double damage, double fireRate,
-      int size) {
+  Weapon(double x, double y, int width, int height, String name, BufferedImage sprite, double damage, double fireRate,
+      int size, int clipSize) {
     this.damage = damage;
     this.fireRate = fireRate;
     this.size = size;
+    this.clipSize = clipSize;
   }
+  
+  abstract void shoot(Angle angle, Vector playerPos, ArrayList<Projectile> projectilesList);
+  
+  abstract void moveProjectile(ArrayList<Projectile> projectilesList);
+
 }
