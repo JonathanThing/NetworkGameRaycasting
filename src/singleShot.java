3620bf2ce967428cfa3d9749738891b6319e294f
ShivanshP1 <97240677+ShivanshP1@users.noreply.github.com>
2022-01-20 16:11:42 -0500

    Small fix

--- src/singleShot.java
+++ src/singleShot.java
@@ -4,20 +4,32 @@ import java.util.ArrayList;
 
 
 public class singleShot extends Weapon{
-	long lastFire;
-	long timer;
-	long cooldown;
+	private long lastFire;
+	private long timer;
+	private long cooldown;
+	private boolean reloading = false;
 	
 	void shoot(Angle angle, Vector playerPos, ArrayList<Projectile> projectilesList) {
         cooldown = setCooldown(this.lastFire, this.timer);
-        if(this.cooldown < 5) {    
-    		double yComponent = -1* Math.sin(angle.getAngleValue());
-    		double xComponent = -1* Math.cos(angle.getAngleValue());		
-    		projectilesList.add(new Projectile(new Vector(playerPos.getX(),playerPos.getY()), 20, 20, "Bullet", angle, null, 10, 10, xComponent, yComponent));
+        if(projectilesList.size() == this.getAmmoSize()) {
+        	projectilesList.clear();
+        	reloading = true;
         }
-        else if(this.cooldown > this.getFireRate()) {
-        	lastFire = System.currentTimeMillis();
-    	    cooldown = 0;
+        else {
+        	if(reloading == false) {
+        		if(this.cooldown < 5) {    
+            		double yComponent = -1* Math.sin(angle.getAngleValue());
+            		double xComponent = -1* Math.cos(angle.getAngleValue());		
+            		projectilesList.add(new Projectile(new Vector(playerPos.getX(),playerPos.getY()), 20, 20, "Bullet", angle, null, 10, 10, xComponent, yComponent));
+                }
+                else if(this.cooldown > this.getFireRate()) {
+                	lastFire = System.currentTimeMillis();
+            	    cooldown = 0;           
+                }  		    		
+        	}
+        	if (this.getReloadTime() < cooldown) {
+        		reloading = false;
+        	}
         }
 	}
 	
@@ -33,8 +45,8 @@ public class singleShot extends Weapon{
 	}
 
 	singleShot(double x, double y, int width, int height, String name, BufferedImage sprite, double damage, double fireRate,
-			int size, int clipSize) {
-		super(x, y, width, height, name, sprite, damage, fireRate, size, clipSize);
+			int size, int clipSize, int reloadTime) {
+		super(x, y, width, height, name, sprite, damage, fireRate, size, clipSize, reloadTime);
 	}
 
 }
