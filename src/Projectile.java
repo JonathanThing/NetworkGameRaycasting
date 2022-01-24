4b90329b0514f83c657c0483c60f240d740d89db
ShivanshP1 <97240677+ShivanshP1@users.noreply.github.com>
2022-01-24 10:33:23 -0500

    small change

--- src/Projectile.java
+++ src/Projectile.java
@@ -16,12 +16,11 @@ class Projectile extends Entity {
   
   public void draw(Graphics g, double offSetX, double offSetY) {
       //System.out.println("drawing projectile");
-      g.fillRect((int) (getPosition().getX() - getWidth() / 2 - offSetX), (int) (getPosition().getY() - getHeight() / 2 - offSetY), getWidth(), getHeight()); //draw the projectile
+      g.fillRect((int) (getPosition().getX() - getWidth() / 2 + offSetX), (int) (getPosition().getY() - getHeight() / 2 + offSetY), getWidth(), getHeight()); //draw the projectile
   }
 
-  Projectile(Vector position, int width, int height, String name, Angle angle, BufferedImage sprite, int health,
-      double speed, double changeX, double changeY) {
-    super(position, width, height, name, angle, sprite, health, speed);
+  Projectile(Vector position, int width, int height, String name, Angle angle, TextureManager sprites, double health, double speed, double spriteZOffset, double spriteScale, double changeX, double changeY) {
+    super(position, width, height, name, angle, sprites, health, speed, spriteZOffset, spriteScale);
     this.changeX = changeX;
     this.changeY = changeY;
   }
