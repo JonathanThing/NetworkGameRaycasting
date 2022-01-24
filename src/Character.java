c925324033ae6e6a8f1e6f53d14f8e125e087b5d
ShivanshP1 <97240677+ShivanshP1@users.noreply.github.com>
2022-01-24 10:04:45 -0500

    small change

--- src/Character.java
+++ src/Character.java
@@ -6,14 +6,15 @@ abstract class Character extends Entity {
   private Weapon weapon;
   private ArrayList<Projectile> projectilesList = new ArrayList<Projectile>();
 
-  Character(Vector position, int width, int height, String name, Angle angle, BufferedImage sprite, double health,
-      double speed, Weapon weapon) {
-    super(position, width, height, name, angle, sprite, health, speed);
+  Character(Vector position, int width, int height, String name, Angle angle, TextureManager sprites, double health,
+		double speed, double spriteZOffset, double spriteScale, Weapon weapon) {
+    super(position, width, height, name, angle, sprites, health, speed, spriteZOffset, spriteScale);
     this.weapon = weapon;
 
   }
   
-  public ArrayList<Projectile> getProjectilesList() {
+
+public ArrayList<Projectile> getProjectilesList() {
       return projectilesList;
   }
   
