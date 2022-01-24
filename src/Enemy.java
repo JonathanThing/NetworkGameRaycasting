f5f773904afc19ba938769a2a854e9bd52ac1177
ShivanshP1 <97240677+ShivanshP1@users.noreply.github.com>
2022-01-24 10:05:41 -0500

    Small change

--- src/Enemy.java
+++ src/Enemy.java
@@ -2,13 +2,13 @@ import java.awt.image.BufferedImage;
 
 abstract class Enemy extends Character {
     
-    Enemy(Vector position, int width, int height, String name, Angle angle, BufferedImage sprite, double health,
-          double speed, Weapon weapon) {
-        super(position, width, height, name, angle, sprite, health, speed, weapon);
-    }
+    Enemy(Vector position, int width, int height, String name, Angle angle, TextureManager sprite, double health,
+          double speed,double spriteZOffset, double spriteScale, Weapon weapon) {
+        super(position, width, height, name, angle, sprite, health, speed, spriteZOffset, spriteScale, weapon);
+    } 
 
     public abstract void attack(Player player);
     
     
     
-}
+} 
