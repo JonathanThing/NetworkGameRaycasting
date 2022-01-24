234419137c9045ca1a2034f880f471992827a36d
ShivanshP1 <97240677+ShivanshP1@users.noreply.github.com>
2022-01-24 10:43:51 -0500

    small change

--- src/Weapon.java
+++ src/Weapon.java
@@ -1,5 +1,9 @@
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+
+import javax.imageio.ImageIO;
 
 abstract class Weapon{
 
@@ -12,6 +16,8 @@ abstract class Weapon{
   private long lastFire;
   
   private int reloadTime;
+  
+  TextureManager fireball;
   
   public double getFireRate() {
     return this.fireRate;
@@ -26,25 +32,33 @@ abstract class Weapon{
   }
   
   public int getAmmoSize() {
-	    return this.AmmoSize;
-	  }
+	  return this.AmmoSize;
+  }
+  
   public int getReloadTime() {
-	    return this.reloadTime;
-	  }
+	  return this.reloadTime;
+  }
+
   long setCooldown(long lastFire, long timer) {
 		return(timer - lastFire) / 10;
 	}
-  
+
   abstract void shoot(Angle angle, Vector playerPos, ArrayList<Projectile> projectilesList);
   
   abstract void moveProjectile(ArrayList<Projectile> projectilesList);
   
-  Weapon(double x, double y, int width, int height, String name, BufferedImage sprite, double damage, double fireRate,
+  Weapon(double x, double y, int width, int height, String name, TextureManager sprite, double damage, double fireRate,
       int AmmoSize, int reloadTime) {
     this.damage = damage;
     this.fireRate = fireRate;
     this.AmmoSize = AmmoSize;
     this.reloadTime = reloadTime;
+    try {
+		fireball = new TextureManager(ImageIO.read(new File("C://FireBallAnimation.png")));
+	} catch (IOException e1) {
+		System.out.println("failed to get image");
+		e1.printStackTrace();
+	}
   }
 
 
