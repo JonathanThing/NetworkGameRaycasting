74b84e4221027c806e6ff1c23cbdae32b6f9b3b5
ShivanshP1 <97240677+ShivanshP1@users.noreply.github.com>
2022-01-24 10:24:47 -0500

    Added animation and fixed bullet spread

--- src/multiShot.java
+++ src/multiShot.java
@@ -20,15 +20,15 @@ public class multiShot extends Weapon{
                 		double yComponent = 0;
                 		double xComponent = 0;
             			if(i%2 == 1) {
-                    		yComponent =  -1*Math.sin((angle.getAngleValue()-0.1*i));
-                    		xComponent =  -1*Math.cos((angle.getAngleValue()-0.1*i));	
+                    		yComponent =  -1*Math.sin((angle.getValue()-0.05*i));
+                    		xComponent =  -1*Math.cos((angle.getValue()-0.05*i));	
             			}
             			else if(i%2 ==0) {
-                    		yComponent =  -1*Math.sin((angle.getAngleValue()-0.1*i));
-                    		xComponent =  -1*Math.cos((angle.getAngleValue()-0.1*i));		            				
+                    		yComponent =  -1*Math.sin((angle.getValue()-0.05*i));
+                    		xComponent =  -1*Math.cos((angle.getValue()-0.05*i));		            				
             			}
 
-            			projectilesList.add(new Projectile(new Vector(playerPos.getX(),playerPos.getY()), 20, 20, "Bullet", angle, null, 10, 10, xComponent, yComponent));
+            			projectilesList.add(new Projectile(new Vector(playerPos.getX(),playerPos.getY()), 20, 20, "Bullet", angle, fireball, 10, 10, 0, 1, xComponent, yComponent));
             		}
                 }
                 else if(this.cooldown > this.getFireRate()) {
@@ -54,7 +54,7 @@ public class multiShot extends Weapon{
 	    }
 	}
 
-	multiShot(double x, double y, int width, int height, String name, BufferedImage sprite, double damage, double fireRate, 
+	multiShot(double x, double y, int width, int height, String name, TextureManager sprite, double damage, double fireRate, 
 			int clipSize, int reloadTime, int numOfShots) {
 		super(x, y, width, height, name, sprite, damage, fireRate, clipSize, reloadTime);
 		this.shots = numOfShots;
