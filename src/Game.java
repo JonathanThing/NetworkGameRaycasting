75949de86b68ace591ce0214c5ae277fa00b7319
ShivanshP1 <97240677+ShivanshP1@users.noreply.github.com>
2022-01-24 10:06:57 -0500

    Added Jonathan's code

--- src/Game.java
+++ src/Game.java
@@ -10,6 +10,7 @@ import java.awt.*;
 import java.awt.event.*;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
 public class Game {
@@ -21,10 +22,11 @@ public class Game {
 	static MyKeyListener keyListener = new MyKeyListener();
 	static MyMouseListener mouseListener = new MyMouseListener();
 	static MyMouseMotionListener mouseMotionListener = new MyMouseMotionListener();
-	static TextureList textures;
-	static TextureList sprites;
-	static Sprite[] test = new Sprite[5];	
-	private static Robot robo;
+	static TextureManager textures;
+	static TextureManager sprites;
+	static TextureManager personDirection;
+	static TextureManager fireball;
+	public static Robot robo;
 	static Level currentLevel = new Level(new int[][]{
 												{2,2,2,1,2,1,2,2,2,2,2,2,2,2,2,2},
 												{2,0,0,0,0,0,0,0,2,0,0,0,0,0,0,2},
@@ -42,11 +44,10 @@ public class Game {
 												{2,0,0,0,0,0,0,0,2,0,0,0,0,0,0,2},
 												{2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2}}); 
 
-	static Zombie zombie = new Zombie(new Vector(400, 300), 30, 30, "skeleton", new Angle(2), null, 100, 4, null); 
-	static Skeleton skeleton = new Skeleton(new Vector(200, 200), 30, 30, "skeleton", new Angle(2), null, 100, 4, null); 
-	static Player player = new Player(new Vector ((3)*Const.BOXSIZE - Const.BOXSIZE/2, (2)*Const.BOXSIZE - Const.BOXSIZE/2), 10, 10, "player", new Angle(3*Math.PI/2), null, 100, 4, null);
+	static Player player = new Player(new Vector ((3)*Const.BOXSIZE - Const.BOXSIZE/2, (2)*Const.BOXSIZE - Const.BOXSIZE/2), 10, 10, "player", new Angle(3*Math.PI/2), null, 100, 4, 20,0.75, null);
 	static boolean up, down, left, right, turnRight, turnLeft, shooting;
-	static Vector cameraOffset = new Vector(0,0);
+	static Vector cameraOffset = new Vector(0,0);	
+	static ArrayList<Entity> entities = new ArrayList<Entity>();
 
 	// --------------------------------------------------------------------------
 	// declare the properties of all game objects here
@@ -54,74 +55,93 @@ public class Game {
 
 	// ------------------------------------------------------------------------------
 	public static void main(String[] args) {
-		try {
-            robo = new Robot();
-        } catch (AWTException e) {
-            e.printStackTrace();
-        }
-		try {
-			textures = new TextureList(ImageIO.read(new File("D://textures.png")));
-			sprites = new TextureList(ImageIO.read(new File("D://spriteSheet.png")));
-		} catch (IOException e) {
-			System.out.println("failed to get image");
-			e.printStackTrace();
-		}
-		
-		test[0] = new Sprite(new Vector ((3)*Const.BOXSIZE - Const.BOXSIZE/2, (3)*Const.BOXSIZE - Const.BOXSIZE/2), sprites.getSingleTexture(5),20,0.75);
-		test[1] = new Sprite(new Vector ((2)*Const.BOXSIZE - Const.BOXSIZE/2, (2)*Const.BOXSIZE - Const.BOXSIZE/2), sprites.getSingleTexture(4),0,1);
-		test[2] = new Sprite(new Vector ((3)*Const.BOXSIZE - Const.BOXSIZE/2, (2)*Const.BOXSIZE - Const.BOXSIZE/2), sprites.getSingleTexture(4),0,1);
-		test[3] = new Sprite(new Vector ((2)*Const.BOXSIZE - Const.BOXSIZE/2, (4)*Const.BOXSIZE - Const.BOXSIZE/2), sprites.getSingleTexture(4),0,1);
-		test[4] = new Sprite(new Vector ((4)*Const.BOXSIZE - Const.BOXSIZE/2, (3)*Const.BOXSIZE - Const.BOXSIZE/2), sprites.getSingleTexture(1),20,0.75);
-		
-		gameWindow = new JFrame("Game Window");
-		gameWindow.setSize(Const.TRUE_WIDTH, Const.TRUE_HEIGHT);
-		gameWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		canvas = new GraphicsPanel();
-		canvas.addMouseListener(mouseListener);
-		canvas.addMouseMotionListener(mouseMotionListener);
-		canvas.addKeyListener(keyListener);
-		gameWindow.add(canvas);
+			try {
+	            robo = new Robot();
+	        } catch (AWTException e) {
+	            e.printStackTrace();
+	        }
+            try {
+    			textures = new TextureManager(ImageIO.read(new File("C://WallTextures.png")));
+    			sprites = new TextureManager(ImageIO.read(new File("C://spriteSheet.png")));
+    			personDirection = new TextureManager(ImageIO.read(new File("C://PersonDirectionAnimation.png")));
+    			fireball = new TextureManager(ImageIO.read(new File("C://FireBallAnimation.png")));
+    		} catch (IOException e1) {
+    			System.out.println("failed to get image");
+    			e1.printStackTrace();
+    		}
+    		
+//    		entities.add(new Zombie(new Vector(400, 300), 30, 30, "skeleton", new Angle(2), sprites.getSingleTexture(0), 100, 4, 20,0.75, null));
+//    		entities.add(new Skeleton(new Vector((3)*Const.BOXSIZE - Const.BOXSIZE/2, (4)*Const.BOXSIZE - Const.BOXSIZE/2), 30, 30, "skeleton", new Angle(Math.PI/2), personDirection, 100, 4, 0,0.75, null));
+    		entities.add(new Skeleton(new Vector((3)*Const.BOXSIZE - Const.BOXSIZE/2, (6)*Const.BOXSIZE - Const.BOXSIZE/2), 30, 30, "skeleton", new Angle(3*Math.PI/2), personDirection, 100, 4, 0,0.75, null, fireball));
+//    		entities.add(new Skeleton(new Vector((6)*Const.BOXSIZE - Const.BOXSIZE/2, (2)*Const.BOXSIZE - Const.BOXSIZE/2), 30, 30, "skeleton", new Angle(Math.PI), personDirection, 100, 4, 0,0.75, null));
+//    		entities.add(new Skeleton(new Vector((6)*Const.BOXSIZE - Const.BOXSIZE/2, (5)*Const.BOXSIZE - Const.BOXSIZE/2), 30, 30, "skeleton", new Angle(Math.PI/2), personDirection, 100, 4, 0,0.75, null));
+//    		entities.add(new Skeleton(new Vector((8)*Const.BOXSIZE - Const.BOXSIZE/2, (4)*Const.BOXSIZE - Const.BOXSIZE/2), 30, 30, "skeleton", new Angle(2*Math.PI), personDirection, 100, 4, 0,0.75, null));
 
-		mapWindow = new JFrame("Map");
-		mapWindow.setSize(Const.WIDTH, Const.HEIGHT);
-		mapWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		mapThing = new MapPanel();
-		mapWindow.add(mapThing);
+    		entities.add(player);
+    		gameWindow = new JFrame("Game Window");
+    		gameWindow.setSize(Const.SCREEN_WIDTH, Const.SCREEN_HEIGHT);
+    		gameWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    		canvas = new GraphicsPanel();
+    		canvas.addMouseListener(mouseListener);
+    		canvas.addMouseMotionListener(mouseMotionListener);
+    		canvas.addKeyListener(keyListener);
+    		gameWindow.add(canvas);
 
-		rayCaster = new RayCaster(textures);
+    		mapWindow = new JFrame("Map");
+    		mapWindow.setSize(Const.WIDTH, Const.HEIGHT);
+    		mapWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    		mapThing = new MapPanel();
+    		mapWindow.add(mapThing);
 
-		mapWindow.setVisible(true);
-		gameWindow.setVisible(true);
-		runGameLoop();
+    		rayCaster = new RayCaster(textures);
+            rayCaster.updateInformation(player, cameraOffset, currentLevel, Math.PI/2);
+    		mapWindow.setVisible(true);
+    		gameWindow.setVisible(true);
+    		runGameLoop();
 
-	} // main method end
+    	} // main method end
 
 	// ------------------------------------------------------------------------------
 	public static void runGameLoop() {
 		while (true) {
-			gameWindow.repaint();
-			mapWindow.repaint();
 			try {
 				Thread.sleep(25);
-			} catch (Exception e) {}
+			} catch (Exception e) {		
+			}
 			player.movement(up, down, left, right, turnLeft, turnRight, currentLevel);
-			
-            //skeleton.shoot(player);
-			player.moveProjectiles();
-            skeleton.moveProjectile();
-            zombie.attack(player);
+            player.moveProjectiles();
             if (shooting){
-                player.shoot();
-                skeleton.shoot(player);
+                player.shoot(fireball);
             }
+            
+            for (Entity thing : entities) {
+            	if (thing instanceof Skeleton) {
+	            	((Skeleton) thing).moveProjectile();
+		            if (shooting){
+		            	((Skeleton) thing).attack(player);
+		            }	            
+		            
+		        	long currentTime = System.currentTimeMillis();       	
+		        	if (thing.getSprites().getLastAnimationChange() + 500 <= currentTime) {
+		        		thing.getSprites().setLastAnimationChange(currentTime);
+		        		thing.getSprites().changeAnimationNumber(1);
+		        	}
+            	} else if (thing instanceof Zombie) {
+                	((Zombie) thing).attack(player);
+            	}
+            }    
+            
+            rayCaster.updateInformation(player, cameraOffset, currentLevel, Const.FOV);
+			gameWindow.repaint();
+			mapWindow.repaint();
 		}
 	} // runGameLoop method end
 	// ------------------------------------------------------------------------------
 	
 	private static void MouseController(MouseEvent e) {
 		gameWindow.setCursor(Cursor.CROSSHAIR_CURSOR);
-		final int midX = Const.TRUE_WIDTH/2;
-		final int midY = Const.TRUE_HEIGHT/2;
+		final int midX = Const.WIDTH/2;
+		final int midY = Const.HEIGHT/2;
 		int adjfactor = -7;		
         
         robo.mouseMove(midX + gameWindow.getX(), midY + gameWindow.getY());
@@ -153,18 +173,31 @@ public class Game {
 
 		public void paintComponent(Graphics g) {
 			super.paintComponent(g); // required
-
+			
 			Graphics2D g2 = (Graphics2D) g;
 			g2.setColor(Color.GRAY);
-			g2.fillRect(0, 0, Const.WIDTH, Const.HEIGHT / 2);
-			g2.setColor(Color.BLACK);
-			g2.fillRect(0,Const.HEIGHT/2,Const.WIDTH,Const.HEIGHT);
-			rayCaster.rayCast(g2, false, player.getPosition(), player.getAngle(), cameraOffset, currentLevel);
-			rayCaster.drawSprite(g2, test);
+			g2.fillRect(0, 0, Const.WIDTH+1, Const.HEIGHT / 2);
+			g2.setColor(Color.GRAY.darker().darker());
+			g2.fillRect(0,Const.HEIGHT/2,Const.WIDTH+1,Const.HEIGHT);
+//			rayCaster.rayCastFloors(g2);
+			rayCaster.rayCastWalls(g2, false);
+			
+			ArrayList<Entity> allEntities = new ArrayList<Entity>();
+			
+			for (Entity thing : entities) {
+				allEntities.add(thing);
+				if (thing instanceof Character) {
+					for (Entity projectile : ((Character) thing).getProjectilesList()) {
+						allEntities.add(projectile);
+					}					
+				}
+			}
+
+			rayCaster.drawSprite(g2, allEntities);
 
 		} // paintComponent method end
 	} // GraphicsPanel class end
-
+	
 	static class MapPanel extends JPanel {
 		public MapPanel() {
 			setFocusable(true);
@@ -175,8 +208,6 @@ public class Game {
 			super.paintComponent(g); // required
 
 			Graphics2D g2 = (Graphics2D) g;
-
-			g2.rotate(0);
 			g2.setStroke(new BasicStroke(4));
 			g2.setColor(Color.BLACK);
 			for (int rows = 0; rows < currentLevel.getRows(); rows++) {
@@ -196,27 +227,29 @@ public class Game {
 				}
 			}
 
-			rayCaster.rayCast(g2, true, player.getPosition(), player.getAngle(),
-					cameraOffset, currentLevel);
+			rayCaster.rayCastWalls(g2, true);
 			
-			g.setColor(Color.GREEN);
-            g2.fillRect((int) (zombie.getPosition().getX() - zombie.getWidth() / 2 + cameraOffset.getX()),
-                        (int) (zombie.getPosition().getY() - zombie.getHeight() / 2 + cameraOffset.getY()), zombie.getWidth(),
-                        zombie.getHeight());
-            
-            g.setColor(Color.RED);
+			for (Entity entity : entities) {
+
+				if (entity instanceof Skeleton) {
+					g.setColor(Color.RED);
+		            g.setColor(Color.RED);
+		            ((Skeleton) entity).drawEnemyProjectile(g2, cameraOffset.getX(), cameraOffset.getY());
+
+            	} else if (entity instanceof Zombie) {
+            		g.setColor(Color.GREEN);
+            	}
+								
+	            g2.fillRect((int) (entity.getPosition().getX() - entity.getWidth() / 2 + cameraOffset.getX()),
+	                        (int) (entity.getPosition().getY() - entity.getHeight() / 2 + cameraOffset.getY()), entity.getWidth(),
+	                        entity.getHeight());
+	            
+	            g2.drawLine((int) (entity.getPosition().getX() + cameraOffset.getX()), (int) (entity.getPosition().getY() + cameraOffset.getY()), (int) (entity.getPosition().getX() + cameraOffset.getX() + Math.cos(entity.getAngle().getValue())*200), (int) (entity.getPosition().getY() + cameraOffset.getY() - Math.sin(entity.getAngle().getValue())*200));
+			}
+			
+			g.setColor(Color.RED);
             player.drawPlayerProjectile(g2, cameraOffset.getX(), cameraOffset.getY());
-            
-            g2.fillRect((int) (skeleton.getPosition().getX() - skeleton.getWidth() / 2 + cameraOffset.getX()),
-                        (int) (skeleton.getPosition().getY() - skeleton.getHeight() / 2 + cameraOffset.getY()), skeleton.getWidth(),
-                        skeleton.getHeight());
-            
-            skeleton.drawEnemyProjectile(g2, cameraOffset.getX(), cameraOffset.getY());
-			
-			g.setColor(Color.ORANGE);	
-			g2.rotate(-player.getAngle().getAngleValue(), player.getPosition().getX()+ cameraOffset.getX(), player.getPosition().getY()+ cameraOffset.getY());
-			g2.fillRect((int) (player.getPosition().getX() - player.getWidth() / 2 + cameraOffset.getX()), (int) (player.getPosition().getY() - player.getHeight() / 2 + cameraOffset.getY()), player.getWidth(), player.getHeight());
-			
+
 		
 		} // paintComponent method end
 	} // GraphicsPanel class end
