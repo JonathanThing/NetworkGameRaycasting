596c154bed286628c595fb90a6d01a42274d782d
ShivanshP1 <97240677+ShivanshP1@users.noreply.github.com>
2022-01-24 10:06:29 -0500

    Small Update

--- src/Environment.java
+++ src/Environment.java
@@ -2,8 +2,8 @@ import java.awt.image.BufferedImage;
 
 public abstract class Environment extends GameObject {
 
-  Environment(Vector position, int width, int height, String name, Angle angle, BufferedImage sprite) {
-    super(position, width, height, name, angle, sprite);
+  Environment(Vector position, int width, int height, String name, Angle angle, TextureManager sprites) {
+    super(position, width, height, name, angle, sprites);
 
   }
 }
