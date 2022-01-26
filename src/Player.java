d33cfa39912567c6af875f2ad6b28ee882b795ce
ShivanshP1 <97240677+ShivanshP1@users.noreply.github.com>
2022-01-26 02:58:32 -0500

    projectile update

--- src/Player.java
+++ src/Player.java
@@ -1,147 +1,99 @@
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 import java.lang.Math;
 
-import javax.imageio.ImageIO;
-
-import java.awt.Graphics;
-
 class Player extends Character {
-	private boolean slot1 = false;
-	private boolean slot2 = false;
-	private boolean slot3 = false;
-	
-	singleShot pistol = new singleShot(this.getPosition().getX(),this.getPosition().getX(),5,5,"revolver",null,40,60,6,200);
-	singleShot ar = new singleShot(this.getPosition().getX(),this.getPosition().getX(),5,5,"AR",null,10,10,30,300);
-	singleShot sniper = new singleShot(this.getPosition().getX(),this.getPosition().getX(),5,5,"sniper",null,100,100,4,500);
-	
-	multiShot autoShotgun = new multiShot(this.getPosition().getX(),this.getPosition().getX(),5,5,"Auto Shotgun",null,20,20,8,500,3);
-	multiShot Shotgun = new multiShot(this.getPosition().getX(),this.getPosition().getX(),5,5,"Shotgun",null,35,40,4,200,5);
-	
-    public void shoot(TextureManager sprite){ //BufferedImage sprite) {                
-    	if(this.slot1 == true) {
-    		pistol.shoot(getAngle(), getPosition(), getProjectilesList());
-    	}
-    	else if(this.slot2 == true) {
-        	Shotgun.shoot(getAngle(), getPosition(),this.getProjectilesList());
-    	}
-    	else if(this.slot3 == true) {
-    		ar.shoot(getAngle(), getPosition(),this.getProjectilesList());
-    	}
-    }  
+    private int ammo;
+    singleShot pistol = new singleShot(this.getPosition().getX(), this.getPosition().getY(),5,5, "pistol", null, 40, 40, 6, 40);
+    //multiShot shotgun = new multiShot(this.getPosition().getX(), this.getPosition().getY(),5,5,"shotgun",null,30,50,4,60,3);
+    //dmg, firerate, bullets, reloadtime
     
-    public void moveProjectiles() {
-    	if(this.slot1 == true) {
-    		pistol.moveProjectile(this.getProjectilesList());
-    		//this.getProjectilesList().clear();
-    	}
-    	else if(this.slot2 == true) {
-        	Shotgun.moveProjectile(this.getProjectilesList());
-        	//this.getProjectilesList().clear();
-    	}
-    	else if(this.slot3 == true) {
-    		ar.moveProjectile(this.getProjectilesList());
-    		//this.getProjectilesList().clear();
-    	}
-    }
-    
-    public void drawPlayerProjectile(Graphics g, double offSetX, double offSetY) {
-        for (int i = 0; i < getProjectilesList().size(); i++) { //loop through arrayList
-            (getProjectilesList().get(i)).draw(g, offSetX, offSetY); //draws the projectile
+    public void shoot(long timer) {    		
+    		pistol.shoot(getAngle(), this.getPosition().clone(), timer);
+      }
+
+    public synchronized void movement(boolean up, boolean down, boolean left, boolean right, boolean turnLeft,
+            boolean turnRight, Level map, int deltaX) {
+
+        double xRaw = 0;
+        double yRaw = 0;
+
+        if (up) {
+            yRaw += 1;
+        }
+
+        if (down) {
+            yRaw -= 1;
+        }
+
+        if (right) {
+            xRaw += 1;
+        }
+
+        if (left) {
+            xRaw -= 1;
+        }
+
+        if (turnLeft) {
+            this.getAngle().changeValue(Math.toRadians(5));
+        }
+
+        if (turnRight) {
+            this.getAngle().changeValue(Math.toRadians(-5));
+        }
+
+        double forwardAngle = this.getAngle().getValue();
+        double sideAngle = Angle.checkLimit(forwardAngle - Math.PI / 2);
+
+        Vector forwardVector = new Vector(Math.cos(forwardAngle) * yRaw, -Math.sin(forwardAngle) * yRaw);
+
+        Vector sideVector = new Vector(Math.cos(sideAngle) * xRaw, -Math.sin(sideAngle) * xRaw);
+
+        Vector movementVector = forwardVector.add(sideVector).normalized().multiplyByScalar(this.getSpeed());
+
+        if (!movementVector.isZero()) {
+
+            Vector futurePosition = this.getPosition().add(movementVector.multiplyByScalar(4));
+
+            // door collision
+            if (map.getMapTile((int) futurePosition.getY() / Const.BOX_SIZE,
+                    (int) futurePosition.getX() / Const.BOX_SIZE) == 4) {
+                map.setMapTile((int) futurePosition.getY() / Const.BOX_SIZE,
+                        (int) futurePosition.getX() / Const.BOX_SIZE,
+                        0);
+            }
+
+            // wall side collision
+            futurePosition = this.getPosition().add(movementVector.multiplyByScalar(2));
+            if (map.getMapTile((int) this.getPosition().getY() / Const.BOX_SIZE,
+                    (int) futurePosition.getX() / Const.BOX_SIZE) < 1) {
+                this.moveLeft(movementVector.getX());
+            }
+
+            // wall forward collision
+            if (map.getMapTile((int) futurePosition.getY() / Const.BOX_SIZE,
+                    (int) this.getPosition().getX() / Const.BOX_SIZE) < 1) {
+                this.moveDown(movementVector.getY());
+            }
         }
     }
-   
-    public void movement(boolean up, boolean down, boolean left, boolean right, boolean turnLeft, boolean turnRight,
-            Level map) {
 
-	double xRaw = 0;
-	double yRaw = 0;
-	
-	if (up) {
-	yRaw += 1;
-	}
-	
-	if (down) {
-	yRaw -= 1;
-	}
-	
-	if (right) {
-	xRaw += 1;
-	}
-	
-	if (left) {
-	xRaw -= 1;
-	}
-	
-	if (turnLeft) {
-	this.getAngle().changeValue(Math.toRadians(5));
-	}
-	
-	if (turnRight) {
-	this.getAngle().changeValue(Math.toRadians(-5));
-	}
-	
-	double forwardAngle = this.getAngle().getValue();
-	double sideAngle = Angle.checkLimit(forwardAngle - Math.PI / 2);
-	
-	Vector forwardVector = new Vector(Math.cos(forwardAngle) * yRaw, -Math.sin(forwardAngle) * yRaw);
-	
-	Vector sideVector = new Vector(Math.cos(sideAngle) * xRaw, -Math.sin(sideAngle) * xRaw);
-	
-	Vector movementVector = forwardVector.add(sideVector).normalized().multiplyByScalar(this.getSpeed());
-	
-		if (!movementVector.isZero()) {
-		
-			Vector futurePosition = this.getPosition().add(movementVector.multiplyByScalar(4));
-			
-			// door collision
-			if (map.getMapTile((int) futurePosition.getY() / Const.BOXSIZE,
-			                  (int) futurePosition.getX() / Const.BOXSIZE) == 4) {
-			   map.setMapTile((int) futurePosition.getY() / Const.BOXSIZE, (int) futurePosition.getX() / Const.BOXSIZE,
-			                  0);
-			}
-			
-			// wall side collision
-			futurePosition = this.getPosition().add(movementVector.multiplyByScalar(2));
-			if (map.getMapTile((int) this.getPosition().getY() / Const.BOXSIZE,
-			                  (int) futurePosition.getX() / Const.BOXSIZE) < 1) {
-			   this.moveLeft(movementVector.getX());
-			}
-			
-			// wall forward collision
-			if (map.getMapTile((int) futurePosition.getY() / Const.BOXSIZE,
-			                  (int) this.getPosition().getX() / Const.BOXSIZE) < 1) {
-			   this.moveDown(movementVector.getY());
-			}
-		}
-	}
-	
-	Player(Vector position, int width, int height, String name, Angle angle, TextureManager sprites, double health,
-	double speed, double spriteZOffset, double spriteScale, Weapon weapon) {
-	super(position, width, height, name, angle, sprites, health, speed, spriteZOffset, spriteScale, weapon); // calls the constructor in the
-	// character super
-	// class
+    public void run() {
+    }
+
+    Player(Vector position, int width, int height, String name, Angle angle, TextureManager sprite, double health,
+            double speed, double spriteZOffset, double spriteScale, Weapon weapon) {
+        super(position, width, height, name, angle, sprite, health, speed, spriteZOffset, spriteScale, weapon); 
+        ammo = 40;
+    }
+
+	public int getAmmo() {
+		return ammo;
 	}
 
-    public boolean getSlot1() {
-    	return this.slot1;
-    }
-    public boolean setSlot2() {
-    	return this.slot2;
-    }
-    public boolean getSlot3() {
-    	return this.slot3;
-    }
-    
-    public void setSlot1(boolean Slot) {
-    	this.slot1 = Slot;
-    }
-    public void setSlot2(boolean Slot) {
-    	this.slot2 = Slot;
-    }
-    public void setSlot3(boolean Slot) {
-    	this.slot3 = Slot;
-    }
-    
+	public void setAmmo(int ammo) {
+		this.ammo = ammo;
+	}
+	
+	public void changeAmmo(int change) {
+		this.ammo += change;
+	}
 }
