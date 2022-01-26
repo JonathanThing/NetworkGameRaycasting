a5d325c2cd9f871886ec9d9e8e2248d3f1884819
Wagee-H <97238088+Wagee-H@users.noreply.github.com>
2022-01-25 19:23:11 -0500

    Delete src directory

--- src/Door.java
+++
@@ -1,9 +0,0 @@
-import java.awt.image.BufferedImage;
-
-public class Door extends Environment {
-    BufferedImage sprite;
-    Door(Vector position, String name, BufferedImage sprite) {
-        super(position, Const.BOX_SIZE, Const.BOX_SIZE, name, null, null);
-        this.sprite = sprite;
-    }
-}
--- src/Environment.java
+++
@@ -1,6 +0,0 @@
-public abstract class Environment extends GameObject {
-
-    Environment(Vector position, int width, int height, String name, Angle angle, TextureManager sprite) {
-        super(position, width, height, name, angle, sprite);
-    }
-}
